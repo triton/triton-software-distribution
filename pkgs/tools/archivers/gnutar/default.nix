triton/triton-software-distribution

commit 054e6335f23ad5ee742d7e326b6957400a91a764

tar, gzip, diffutils: Allow builds with newer versions of glibc.
This is a follow-up to 77fdc7bb3583606c86b2dd602b76a20a1bf6bc7c ("GNU
Coreutils: Allow builds with newer versions of glibc.").

pkgs/tools/archivers/gnutar/default.nix
@@ -37,4 +37,14 @@ stdenv.mkDerivation (rec {
   # cannot be used as a login shell for now.
 // stdenv.lib.optionalAttrs (stdenv.system == "armv7l-linux") {
   FORCE_UNSAFE_CONFIGURE = 1;
+}
+
+//
+
+{
+  crossAttrs =
+    # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
+    (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
+      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+    });
 })

pkgs/tools/compression/gzip/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (rec {
   name = "gzip-1.4";
 
   src = fetchurl {
@@ -31,3 +31,13 @@ stdenv.mkDerivation rec {
     maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
+
+//
+
+{
+  crossAttrs =
+    # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
+    (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
+      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+    });
+})

pkgs/tools/text/diffutils/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl, coreutils ? null}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation ({
   name = "diffutils-3.2";
   
   src = fetchurl {
@@ -16,3 +16,13 @@ stdenv.mkDerivation {
     description = "Commands for showing the differences between files (diff, cmp, etc.)";
   };
 }
+
+//
+
+{
+  crossAttrs =
+    # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
+    (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
+      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+    });
+})

commit 1b179b03c962dd984a73426da551885e08c526bc

tar, gzip: Adjust the `gets' patch.
Fixes the incorrect patches introduced in
054e6335f23ad5ee742d7e326b6957400a91a764.

pkgs/tools/archivers/gnutar/default.nix
@@ -45,6 +45,6 @@ stdenv.mkDerivation (rec {
   crossAttrs =
     # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
     (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
-      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+      patches = [ ./gets-undeclared.patch ];
     });
 })

pkgs/tools/compression/gzip/default.nix
@@ -38,6 +38,6 @@ stdenv.mkDerivation (rec {
   crossAttrs =
     # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
     (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
-      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+      patches = [ ./gets-undeclared.patch ];
     });
 })