triton/triton-software-distribution

commit c2eded80311e095e53c81dd2ebe867ac03ba7e25

OCaml 4.00.1

sets/all-packages.nix
@@ -2442,7 +2442,7 @@ let
 
   ocaml_3_12_1 = callPackage ../development/compilers/ocaml/3.12.1.nix { };
 
-  ocaml_4_00_0 = callPackage ../development/compilers/ocaml/4.00.0.nix { };
+  ocaml_4_00_1 = callPackage ../development/compilers/ocaml/4.00.1.nix { };
 
   metaocaml_3_09 = callPackage ../development/compilers/ocaml/metaocaml-3.09.nix { };
 

commit 4eee98e8ab5a13675a4bd845abac4fae0f035699

probable fix for failing hydra build since c2eded80311e095e53c81dd2ebe867ac03ba7e25 (ocaml_4_00_0 not found).
I'm not an ocaml user so I don't understand the implications of my change.
It is only meant to get the nix expressions valid again.

sets/all-packages.nix
@@ -2541,7 +2541,7 @@ let
   ocamlPackages_3_10_0 = mkOcamlPackages ocaml_3_10_0 pkgs.ocamlPackages_3_10_0;
   ocamlPackages_3_11_2 = mkOcamlPackages ocaml_3_11_2 pkgs.ocamlPackages_3_11_2;
   ocamlPackages_3_12_1 = mkOcamlPackages ocaml_3_12_1 pkgs.ocamlPackages_3_12_1;
-  ocamlPackages_4_00_0 = mkOcamlPackages ocaml_4_00_0 pkgs.ocamlPackages_4_00_0;
+  ocamlPackages_4_00_0 = mkOcamlPackages ocaml_4_00_1 pkgs.ocamlPackages_4_00_0;
 
   ocaml_make = callPackage ../development/ocaml-modules/ocamlmake { };
 