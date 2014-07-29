triton/triton-software-distribution

commit b69004b6dd35bffc761fa0c545d64c9c0de3335f

python-wrapper: Fix fixup for wrapped argv[0].
Fixes issues introduced by 1dd137931eef07b8f5046cd964854b781539c0d8.

The problem here is that "import sys; sys.argv[0] = ..." is just
appended after the first "#!", which in turn breaks things such as
encoding specifications. A second problem - although not very common -
is when there's another #! within the script.

This should take care of both cases.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/development/python-modules/generic/wrap.sh
@@ -26,7 +26,9 @@ wrapPythonProgramsIn() {
             # dont wrap EGG-INFO scripts since they are called from python
             if echo "$i" | grep -v EGG-INFO/scripts; then
                 echo "wrapping \`$i'..."
-                sed -i "$i" -e "/^#\!/a import sys; sys.argv[0] = '$(basename $i)'"
+                sed -i "$i" -e '1,/#!/ { /^#\!/,/^[^#]/ {
+                    /^[^#]/i import sys; sys.argv[0] = '"'$(basename "$i")'"'
+                }}'
                 wrapProgram "$i" \
                     --prefix PYTHONPATH ":" $program_PYTHONPATH \
                     --prefix PATH ":" $program_PATH

commit ac8696e210acd339808dfd314617329cedd129c2

python-wrapper: Fix handling __future__ imports.
The bazaar package is still broken even with b69004b, because __future__
imports need to be the first imports before anything else. So this time
I'm going to make the sed expression with explicit branching so we can
properly match all the occasions we want to skip and insert the line
modifying sys.argv[0] only _once_ and leave the command block after
that one substitution. So no ugly swaps between hold and pattern space.

The label which is resonsible for not escaping the command block is "r"
and we jump to it as long as we need to skip something from the start of
the file.

While at it, I'm not only skipping every line with __future__ in it but
also backslashes at the end of the line, so for example:

```python
from __future__ import shiny_feature1, \
                       shiny_feature2, \
                       shiny_feature3
```

... will now be properly skipped as well.

Tested against bazaar and nixops.

Thanks to @edolstra for reporting this.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/development/python-modules/generic/wrap.sh
@@ -26,9 +26,12 @@ wrapPythonProgramsIn() {
             # dont wrap EGG-INFO scripts since they are called from python
             if echo "$i" | grep -v EGG-INFO/scripts; then
                 echo "wrapping \`$i'..."
-                sed -i "$i" -e '1,/#!/ { /^#\!/,/^[^#]/ {
-                    /^[^#]/i import sys; sys.argv[0] = '"'$(basename "$i")'"'
-                }}'
+                sed -i "$i" -re '1 {
+                    /^#!/!b; :r
+                    /\\$/{n;n;n;b r}
+                    /__future__|^ *(#.*)?$/{n;b r}
+                    /^ *[^# ]/i import sys; sys.argv[0] = '"'$(basename "$i")'"'
+                }'
                 wrapProgram "$i" \
                     --prefix PYTHONPATH ":" $program_PYTHONPATH \
                     --prefix PATH ":" $program_PATH