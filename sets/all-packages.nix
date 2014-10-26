triton/triton-software-distribution

commit 3193a5079ab6f05303018f66e33d460ef5b3bb3b

youtube-dl: rename attrname youtubeDl -> youtube-dl
So that both attrname and pkgname is the same. Keep the old attrname
around for backward compatibility.

sets/all-packages.nix
@@ -2741,7 +2741,7 @@ let
 
   xvfb_run = callPackage ../tools/misc/xvfb-run { inherit (texFunctions) fontsConf; };
 
-  youtubeDL = callPackage ../tools/misc/youtube-dl { };
+  youtube-dl = callPackage ../tools/misc/youtube-dl { };
 
   zbar = callPackage ../tools/graphics/zbar {
     pygtk = lib.overrideDerivation pygtk (x: {
@@ -12420,6 +12420,7 @@ let
   lttngTools = lttng-tools;  # added 2014-07-31
   lttngUst = lttng-ust;  # added 2014-07-31
   jquery_ui = jquery-ui;  # added 2014-09-07
+  youtubeDl = youtube-dl;  # added 2014-10-26
 
 
 }; in self; in pkgs

commit ef98b3c3e46bd24049d80e57298b1bf32c017abd

youtube-dl: fix typo
Introduced in commit 3193a5079ab6f05303018f66e33d460ef5b3bb3b
("youtube-dl: rename attrname youtubeDl -> youtube-dl"). The old
attribute was spelled with a capital 'L'.

sets/all-packages.nix
@@ -12422,7 +12422,7 @@ let
   lttngTools = lttng-tools;  # added 2014-07-31
   lttngUst = lttng-ust;  # added 2014-07-31
   jquery_ui = jquery-ui;  # added 2014-09-07
-  youtubeDl = youtube-dl;  # added 2014-10-26
+  youtubeDL = youtube-dl;  # added 2014-10-26
 
 
 }; in self; in pkgs