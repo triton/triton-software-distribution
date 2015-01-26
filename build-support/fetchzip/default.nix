triton/triton-software-distribution

commit 0234a64a2ee85aafeeaa35c055988be91acd2dd6

fetchzip: various fixes
This fixes:

 * Passing stripRoot.
 * Archives containing a single file.
 * Archives where the root folder has the same name as one of it's children.

Fixes #5851

build-support/fetchzip/default.nix
@@ -23,22 +23,31 @@ lib.overrideDerivation (fetchurl ({
     ''
       export PATH=${unzip}/bin:$PATH
       mkdir $out
-      cd $out
-      renamed="$TMPDIR/${baseNameOf url}"
+
+      unpackDir="$TMPDIR/unpack"
+      mkdir "$unpackDir"
+      cd "$unpackDir"
+
+      renamed="$TMPDIR/$name"
       mv "$downloadedFile" "$renamed"
       unpackFile "$renamed"
-    ''
-    # FIXME: handle zip files that contain a single regular file.
-    + lib.optionalString stripRoot ''
+
       shopt -s dotglob
-      if [ "$(ls -d $out/* | wc -l)" != 1 ]; then
-        echo "error: zip file must contain a single directory."
+    ''
+    + (if stripRoot then ''
+      if [ $(ls "$unpackDir" | wc -l) != 1 ]; then
+        echo "error: zip file must contain a single file or directory."
         exit 1
       fi
-      fn=$(cd "$out" && echo *)
-      mv $out/$fn/* "$out/"
-      rmdir "$out/$fn"
-    '';
-} // args))
+      fn=$(cd "$unpackDir" && echo *)
+      if [ -f "$unpackDir/$fn" ]; then
+        mv "$unpackDir/$fn" "$out"
+      else
+        mv "$unpackDir/$fn"/* "$out/"
+      fi
+    '' else ''
+      mv "$unpackDir"/* "$out/"
+    '');
+} // removeAttrs args [ "stripRoot" ]))
 # Hackety-hack: we actually need unzip hooks, too
 (x: {nativeBuildInputs = x.nativeBuildInputs++ [unzip];})

commit 0fa6fc88e6ce07ce87b815df57e093f6d6d76e09

fetchzip: fix unpacking regression
This resolves a regression introduced in 0234a64a2ee8, where providing a
name without a proper extension breaks existing uses of fetchzip (they
now fail to unpack). Of particular note, that commit broke all uses of
fetchFromGitHub because it uses a name like so: "${repo}-${rev}-src"

Fixes #5954

build-support/fetchzip/default.nix
@@ -28,7 +28,7 @@ lib.overrideDerivation (fetchurl ({
       mkdir "$unpackDir"
       cd "$unpackDir"
 
-      renamed="$TMPDIR/$name"
+      renamed="$TMPDIR/${baseNameOf url}"
       mv "$downloadedFile" "$renamed"
       unpackFile "$renamed"
 