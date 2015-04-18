NixOS/nixpkgs

libiec61883: Set platforms and update license

pkgs/development/libraries/libiec61883/default.nix
@@ -11,11 +11,12 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ pkgconfig ];
-  
+
   propagatedBuildInputs = [ libraw1394 ];
 
-  meta = { 
+  meta = with stdenv.lib; {
     homepage = http://www.linux1394.org;
-    license = "LGPL";
+    license = licenses.lgpl21;
+    platforms = platforms.linux;
   };
 }

triton/triton-software-distribution

pkgs/l/libiec61883/default.nix
@@ -11,11 +11,12 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ pkgconfig ];
-  
+
   propagatedBuildInputs = [ libraw1394 ];
 
-  meta = { 
+  meta = with stdenv.lib; {
     homepage = http://www.linux1394.org;
-    license = "LGPL";
+    license = licenses.lgpl21Plus;
+    platforms = platforms.linux;
   };
 }