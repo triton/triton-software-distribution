NixOS/nixpkgs

libavc1394: Add platforms and update license

pkgs/development/libraries/libavc1394/default.nix
@@ -14,6 +14,7 @@ stdenv.mkDerivation rec {
   meta = { 
     description = "Programming interface for the 1394 Trade Association AV/C (Audio/Video Control) Digital Interface Command Set";
     homepage = http://sourceforge.net/projects/libavc1394/;
-    license = [ "GPL" "LGPL" ];
+    license = stdenv.lib.licenses.lgpl21;
+    platforms = stdenv.lib.platforms.linux;
   };
 }

triton/triton-software-distribution

pkgs/development/libraries/libavc1394/default.nix
@@ -14,6 +14,7 @@ stdenv.mkDerivation rec {
   meta = { 
     description = "Programming interface for the 1394 Trade Association AV/C (Audio/Video Control) Digital Interface Command Set";
     homepage = http://sourceforge.net/projects/libavc1394/;
-    license = [ "GPL" "LGPL" ];
+    license = stdenv.lib.licenses.lgpl21Plus;
+    platforms = stdenv.lib.platforms.linux;
   };
 }