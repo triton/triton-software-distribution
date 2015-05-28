ternaris/nixpkgs

cygwin: build perl as usual, no sysPerl

pkgs/top-level/all-packages.nix
@@ -3887,9 +3887,7 @@ let
 
   perl520 = callPackage ../development/interpreters/perl/5.20 { };
 
-  # CYGWINTODO
-
-  perl = if system != "i686-cygwin" then perl516 else sysPerl;
+  perl = perl516;
 
   php = php54;
 
@@ -4037,8 +4035,6 @@ let
   };
 
   supercollider_scel = supercollider.override { useSCEL = true; };
-
-  sysPerl = callPackage ../development/interpreters/perl/sys-perl { };
 
   tcl = callPackage ../development/interpreters/tcl { };
 

triton/triton-software-distribution

sets/all-packages.nix
@@ -4807,7 +4807,7 @@ let
     fetchurl = fetchurlBoot;
   };
 
-  perl = if system != "i686-cygwin" then perl520 else sysPerl;
+  perl = perl520;
 
   php = php56;
 
@@ -4964,8 +4964,6 @@ let
   };
 
   supercollider_scel = supercollider.override { useSCEL = true; };
-
-  sysPerl = callPackage ../development/interpreters/perl/sys-perl { };
 
   tcl = tcl-8_6;
   tcl-8_5 = callPackage ../development/interpreters/tcl/8.5.nix { };