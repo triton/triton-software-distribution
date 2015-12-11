triton/triton-software-distribution

commit a2bdcc23270bf0e897e97e61c7969aa56aa32895

python pyside: move callPackage to python-packages.nix
This should make pyside available for all Python versions.

sets/all-packages.nix
@@ -9045,15 +9045,15 @@ let
 
   pyqt4 = pythonPackages.pyqt4;
 
-  pysideApiextractor = callPackage ../development/python-modules/pyside/apiextractor.nix { };
+  pysideApiextractor = pythonPackages.pysideApiextractor;
 
-  pysideGeneratorrunner = callPackage ../development/python-modules/pyside/generatorrunner.nix { };
+  pysideGeneratorrunner = pythonPackages.pysideGeneratorrunner;
 
-  pyside = callPackage ../development/python-modules/pyside { };
+  pyside = pythonPackages.pyside;
 
-  pysideTools = callPackage ../development/python-modules/pyside/tools.nix { };
+  pysideTools = pythonPackages.pysideTools;
 
-  pysideShiboken = callPackage ../development/python-modules/pyside/shiboken.nix { };
+  pysideShiboken = pythonpackages.pysideShiboken;
 
   pyxml = callPackage ../development/python-modules/pyxml { };
 

sets/python-packages.nix
@@ -169,6 +169,16 @@ in modules // {
     qt5 = pkgs.qt5;
   };
 
+  pyside = callPackage ../development/python-modules/pyside { };
+
+  pysideApiextractor = callPackage ../development/python-modules/pyside/apiextractor.nix { };
+
+  pysideGeneratorrunner = callPackage ../development/python-modules/pyside/generatorrunner.nix { };
+
+  pysideShiboken = callPackage ../development/python-modules/pyside/shiboken.nix { };
+
+  pysideTools = callPackage ../development/python-modules/pyside/tools.nix { };
+
   sip = callPackage ../development/python-modules/sip { };
 
   sip_4_16 = callPackage ../development/python-modules/sip/4.16.nix { };

commit 959b98911f9818dc19c117a385930bd841630034

all-packages: Fix typo in "pythonpackages".
Regression introduced by a2bdcc23270bf0e897e97e61c7969aa56aa32895.

All lower-case won't evaluate very well.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

sets/all-packages.nix
@@ -9053,7 +9053,7 @@ let
 
   pysideTools = pythonPackages.pysideTools;
 
-  pysideShiboken = pythonpackages.pysideShiboken;
+  pysideShiboken = pythonPackages.pysideShiboken;
 
   pyxml = callPackage ../development/python-modules/pyxml { };
 