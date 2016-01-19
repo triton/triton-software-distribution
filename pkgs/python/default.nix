4cb06aa27b4a5e97a82b163fd7d607ca6d7b5597
Cody Opel <codyopel@gmail.com>
2016-01-19 01:24:50 -0500

    python: split 2 & 3 into sparate builds

--- pkgs/python/default.nix
+++ pkgs/python/default.nix
@@ -1,11 +1,11 @@
 { stdenv
-, fetchTritonPatch
 , fetchurl
 
 , bzip2
 , db
 , expat
 , gdbm
+, libffi
 , lzma
 , ncurses
 , openssl
@@ -21,11 +21,20 @@
 , self ? null
 }:
 
+/*
+ * Test the following packages when making major changes, as they
+ * look for or link against libpython:
+ * - gst-python 0.10.x & 1.x
+ * - libtorrent-rasterbar
+ * - pycairo
+ */
+
 with {
   inherit (stdenv)
     isLinux;
   inherit (stdenv.lib)
     concatStringsSep
+    optional
     optionals
     optionalString
     versionAtLeast
@@ -50,6 +59,7 @@ let
 in
 
 assert channel != null;
+assert isPy3;
 
 stdenv.mkDerivation rec {
   name = "python-${version}";
@@ -70,46 +80,12 @@ stdenv.mkDerivation rec {
     preferLocalBuild = true;
   };
 
-  patches = optionals isPy2 [
-    # patch python to put zero timestamp into pyc
-    # if DETERMINISTIC_BUILD env var is set
-    (fetchTritonPatch {
-      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
-      file = "python/python-2.7-deterministic-build.patch";
-      sha256 = "7b8ed591008f8f0dafb7f2c95d06404501c84223197fe138df75791f12a9dc24";
-    })
-    (fetchTritonPatch {
-      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
-      file = "python/python-2.7-properly-detect-curses.patch";
-      sha256 = "c0d17df5f1c920699f68a1c87973d626ea8423a4881927b0ac7a20f88ceedcb4";
-    })
-    # Python recompiles a Python if the mtime stored *in* the
-    # pyc/pyo file differs from the mtime of the source file.  This
-    # doesn't work in Nix because Nix changes the mtime of files in
-    # the Nix store to 1.  So treat that as a special case.
-    (fetchTritonPatch {
-      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
-      file = "python/python-2.x-nix-store-mtime.patch";
-      sha256 = "0869ba7b51b1c4b8f9779118a75ce34332a69f41909e5dfcd9305d2a9bcce638";
-    })
-    # Look in C_INCLUDE_PATH and LIBRARY_PATH for stuff.
-    (fetchTritonPatch {
-      rev = "d3fc5e59bd2b4b465c2652aae5e7428b24eb5669";
-      file = "python/python-2.x-search-path.patch";
-      sha256 = "b055ca65c28152cf8ca0f827f972b2b552c4a1df4b4aec1fb59130b153c68794";
-    })
-  ];
-
-  postPatch = ''
-    # improve purity
+  postPatch =
+    /* Prevent setup.py from looking for include/lib
+       directories in impure paths */ ''
     for i in /usr /sw /opt /pkg ; do
       substituteInPlace ./setup.py \
         --replace $i /no-such-path
-    done
-  '' + optionalString (stdenv ? cc && stdenv.cc.libc != null && isPy2) ''
-    for i in Lib/plat-*/regen ; do
-      substituteInPlace $i \
-        --replace /usr/include/ ${stdenv.cc.libc}/include/
     done
   '';
 
@@ -117,14 +93,13 @@ stdenv.mkDerivation rec {
     "--disable-universalsdk"
     "--disable-framework"
     "--enable-shared"
-    "--disable-profiling"
-    (verFlag isPy2 "--disable-toolbox-glue")
-    (verFlag isPy3 "--enable-loadable-sqlite-extensions")
+    #"--disable-profiling"
+    "--disable-toolbox-glue"
+    "--enable-loadable-sqlite-extensions"
     "--enable-ipv6"
-    (verFlag isPy2 "--enable-unicode=ucs4")
-    #(verFlag isPy3 "--enable-big-digits")
-    (wtFlag "gcc" (!stdenv.cc.isClang) null)
-    #(verFlag isPy3 "--with-hash-algorithm")
+    #"--enable-big-digits"
+    #(wtFlag "gcc" (!stdenv.cc.isClang) null)
+    #"--with-hash-algorithm"
     # Flag is not a boolean
     (verFlag (versionAtLeast versionMajor "3.5")
       (if stdenv.cc.isClang then
@@ -133,14 +108,14 @@ stdenv.mkDerivation rec {
          null))
     "--with-system-expat"
     "--with-system-ffi"
-    (verFlag isPy3 "--with-system-libmpdec")
+    #"--with-system-libmpdec"
     #"--with-dbmliborder"
     #"--with-signal-module"
     "--with-threads"
     #"--with-doc-strings"
     #"--with-tsc"
     #"--with-pymalloc"
-    "--without-valgrind"
+    #"--without-valgrind"
     #"--with-fpectl"
     #"--with-libm"
     #"--with-libc"
@@ -152,16 +127,10 @@ stdenv.mkDerivation rec {
   NIX_LDFLAGS = optionalString isLinux "-lgcc_s";
 
   preConfigure = ''
-    # improve purity
-    for i in /usr /sw /opt /pkg ; do
-      substituteInPlace ./setup.py \
-        --replace $i /no-such-path
-    done
-
     configureFlagsArray+=(
       CPPFLAGS="${concatStringsSep " " (map (p: "-I${p}/include") buildInputs)}"
       LDFLAGS="${concatStringsSep " " (map (p: "-L${p}/lib") buildInputs)}"
-      LIBS="-lcrypt ${optionalString (ncurses != null) "-lncurses"}"
+      LIBS="${optionalString (ncurses != null) "-lncurses"}"
     )
   '';
 
@@ -170,35 +139,19 @@ stdenv.mkDerivation rec {
     db
     expat
     gdbm
+    libffi
     lzma
     ncurses
     openssl
     readline
     sqlite
+    stdenv.cc.libc
     zlib
   ];
 
-  # tkinter is disabled
-  modules = "_bsddb,_curses,_curses_panel,_crypt,_gdbm,_sqlite3,_readline";
-
-  postBuild = optionalString isPy2 ''
-    # This uses the python interpreter that was just built to run the
-    # script to build the modules
-    substituteInPlace setup.py --replace 'self.extensions = extensions' \
-      'self.extensions = [ext for ext in self.extensions if ext.name in ["${modules}"]]'
-
-    export C_INCLUDE_PATH="${
-      concatStringsSep ":" (map (p: "${p}/include") buildInputs)}";
-    export LIBRARY_PATH="${
-      concatStringsSep ":" (map (p: "${p}/lib") buildInputs)}";
-
-    export LD_LIBRARY_PATH="$(pwd)";
-
-    ./python setup.py build_ext
-    [ -z "$(find build -name '*_failed.so' -print)" ]
-  '';
-
-  postInstall = ''
+  postInstall =
+    /* Needed for some packages, especially packages that
+       backport functionality to 2.x from 3.x */ ''
     # needed for some packages, especially packages that backport functionality
     # to 2.x from 3.x
     for item in $out/lib/python${versionMajor}/test/* ; do
@@ -208,25 +161,41 @@ stdenv.mkDerivation rec {
         echo $item
       fi
     done
+  '' + ''
     touch $out/lib/python${versionMajor}/test/__init__.py
-
+  '' + ''
     paxmark E $out/bin/python${versionMajor}
-  '' + optionalString isPy2 ''
-    # Install modules
-    dest=$out/lib/python${versionMajor}/site-packages
-    mkdir -p $dest
-    cp -p $(find . -name "*.so") $dest/
-  '' + optionalString isPy2 ''
-    ln -s $out/lib/python${versionMajor}/pdb.py $out/bin/pdb
-    ln -s $out/lib/python${versionMajor}/pdb.py $out/bin/pdb${versionMajor}
-    ln -s $out/share/man/man1/{python2.7.1.gz,python.1.gz}
-  '' + optionalString isPy3 ''
-    # Some programs look for libpython<major.minor>.so
+  '' +
+    /* Some programs look for libpython<major>.<minor>.so */ ''
     if [[ ! -f "$out/lib/libpython${versionMajor}.so" ]] ; then
       ln -sv \
         $out/lib/libpython3.so \
         $out/lib/libpython${versionMajor}.so
     fi
+  '' +
+    /* Symlink include directory */ ''
+    if [[ ! -d "$out/include/python${versionMajor}" ]] ; then
+      ln -sv \
+        $out/include/python${versionMajor}m \
+        $out/include/python${versionMajor}
+    fi
+  '';
+
+  # TODO: move tests to checkPhase
+  preFixup = ''
+    echo "Testing modules"
+    $out/bin/python${versionMajor} -c "import bz2"
+    $out/bin/python${versionMajor} -c "import crypt"
+    $out/bin/python${versionMajor} -c "import ctypes"
+    $out/bin/python${versionMajor} -c "import curses"
+    $out/bin/python${versionMajor} -c "from curses import panel"
+    $out/bin/python${versionMajor} -c "from dbm import gnu"
+    $out/bin/python${versionMajor} -c "import lzma"
+    $out/bin/python${versionMajor} -c "import math"
+    $out/bin/python${versionMajor} -c "import readline"
+    $out/bin/python${versionMajor} -c "import sqlite3"
+    $out/bin/python${versionMajor} -c "import ssl"
+    $out/bin/python${versionMajor} -c "import zlib"
   '';
 
   enableParallelBuilding = true;
@@ -246,8 +215,8 @@ stdenv.mkDerivation rec {
     libPrefix = "python${versionMajor}";
     executable = "python${versionMajor}";
     buildEnv = callPackage ../wrapper.nix { python = self; };
-    isPy2 = versionOlder versionMajor "3.0";
-    isPy3 = versionAtLeast versionMajor "3.0";
+    isPy2 = false;
+    isPy3 = true;
     sitePackages = "lib/${libPrefix}/site-packages";
     interpreter = "${self}/bin/${executable}";
   };
