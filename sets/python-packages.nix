triton/triton-software-distribution

commit d2b13b41e995e260cecc52122eac2caabe8db346

pythonPackages.pygments: 2.0.2 -> 2.1
- Added lexers, styles, formatters.
- New "filename" option for HTML formatter (PR#527).
- Improved performance of the HTML formatter for long lines (PR#504).
- Updated autopygmentize script (PR#445).
- Fixed style inheritance for non-standard token types in HTML
  output.
- Added support for async/await to Python 3 lexer.
- Rewrote linenos option for TerminalFormatter (it's better, but
  slightly different output than before) (#1147).
- Javascript lexer now supports most of ES6 (#1100).
- Cocoa builtins updated for iOS 8.1 (PR#433).
- Combined BashSessionLexer and ShellSessionLexer, new version
  should support the prompt styles of either.
- Added option to pygmentize to show a full traceback on exceptions.
- Fixed incomplete output on Windows and Python 3 (e.g. when using
  iPython Notebook) (#1153).
- Allowed more traceback styles in Python console lexer (PR#253).
- Added decorators to TypeScript (PR#509).
- Fix highlighting of certain IRC logs formats (#1076).

More: https://bitbucket.org/birkenfeld/pygments-main/src/tip/CHANGES

sets/python-packages.nix
@@ -15555,12 +15555,12 @@ in modules // {
   };
 
   pygments = buildPythonPackage rec {
-    version = "2.0.2";
+    version = "2.1";
     name = "Pygments-${version}";
 
     src = pkgs.fetchurl {
       url = "http://pypi.python.org/packages/source/P/Pygments/${name}.tar.gz";
-      sha256 = "0lagrwifsgn0s8bzqahpr87p7gd38xja8f06akscinp6hj89283k";
+      sha256 = "0yx4p3w9lw1kw24zr87xnaqxm007mdxgwa5wjpwnrcfpmxgyz80k";
     };
 
     propagatedBuildInputs = with self; [ docutils ];

commit 5a6f808ad12b3ff7ac1836ccfd9814b4b37ded7a

pythonPackages.pelican: fix tests with Pygments 2.1
Broken in d2b13b41e995e260cecc52122eac2caabe8db346.
I'll open an upstream PR with this patch as well.

sets/python-packages.nix
@@ -14118,6 +14118,8 @@ in modules // {
       sha256 = "1k572anw39rws67mvxl2w6y93y8w8q5smnwc0dd2gnnr16cc2vsh";
     };
 
+    patches = [ ../development/python-modules/pelican-fix-tests-with-pygments-2.1.patch ];
+
     buildInputs = with self; [
       pkgs.glibcLocales
       pkgs.pandoc