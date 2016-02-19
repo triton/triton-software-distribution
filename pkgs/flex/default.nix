0faf4dbedf2078c97c727280e82334aaba6b0667
William A. Kennington III <william@wkennington.com>
2016-02-19 14:10:27 -0800

    flex: Move to all-pkgs

---
+++ pkgs/flex/default.nix
@@ -0,0 +1,30 @@
+{ stdenv
+, bison
+, fetchurl
+, m4
+}:
+
+stdenv.mkDerivation rec {
+  name = "flex-2.6.0";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/flex/${name}.tar.bz2";
+    sha256 = "1sdqx63yadindzafrq1w31ajblf9gl1c301g068s20s7bbpi3ri4";
+  };
+
+  nativeBuildInputs = [
+    bison
+    m4
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = http://flex.sourceforge.net/;
+    description = "A fast lexical analyser generator";
+    maintainers = with maintainers; [
+      wkennington
+    ];
+    platforms = with platforms;
+      x86_64-linux
+      ++ i686-linux;
+  };
+}
