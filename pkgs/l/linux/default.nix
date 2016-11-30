457bc26cf1ea93fd9ef80b91cf5a8c2dbbf94175
William A. Kennington III <william@wkennington.com>
2016-11-29 22:45:36 -0800

    Move linux to all-pkgs

---
+++ pkgs/l/linux/default.nix
@@ -0,0 +1,217 @@
+{ stdenv
+, buildLinux
+, fetchFromGitHub
+, fetchurl
+, git
+, perl
+
+, # Overrides to the kernel config.
+  extraConfig ? ""
+
+, # A list of patches to apply to the kernel.  Each element of this list
+  # should be an attribute set {name, patch} where `name' is a
+  # symbolic name and `patch' is the actual patch.  The patch may
+  # optionally be compressed with gzip or bzip2.
+  kernelPatches ? []
+
+, ignoreConfigErrors ? false
+, extraMeta ? {}
+, channel
+, ...
+}:
+
+let
+
+  sources = {
+    "4.8" = {
+      version = "4.8.11";
+      baseSha256 = "3e9150065f193d3d94bcf46a1fe9f033c7ef7122ab71d75a7fb5a2f0c9a7e11a";
+      patchSha256 = "53d84946cbe641a2a74ed6cbdd35840bb4947cefd34a764c31b7dac5cde5c696";
+    };
+    "testing" = {
+      version = "4.9-rc7";
+      needsGitPatch = true;
+      baseSha256 = "3e9150065f193d3d94bcf46a1fe9f033c7ef7122ab71d75a7fb5a2f0c9a7e11a";
+      patchSha256 = "b12b04c739743d0bebd287cca572caea9bfeea55810d850021533608508c5762";
+    };
+    "bcache" =
+      let
+        date = "2016-11-29";
+      in {
+        version = "4.8.11";
+        patchUrls = [
+          "https://github.com/wkennington/linux/releases/download/bcachefs-${version}-${date}/patch-bcachefs-${version}-${date}.xz"
+        ];
+        baseSha256 = "3e9150065f193d3d94bcf46a1fe9f033c7ef7122ab71d75a7fb5a2f0c9a7e11a";
+        patchSha256 = "94967d7d5facace5ef8952f8c374bf5a3a346fd90d0ef2de8c277dcdba67fafd";
+        features.bcachefs = true;
+      };
+  };
+
+  source = sources."${channel}";
+
+  inherit (source)
+    version;
+
+  inherit (stdenv.lib)
+    elemAt
+    head
+    optionals
+    splitString
+    tail
+    toInt;
+
+  needsGitPatch = source.needsGitPatch or false;
+
+  unpatchedVersion =
+    let
+      rclist = splitString "-" version;
+      isRC = [ ] != tail rclist;
+      vlist = splitString "." (head rclist);
+      minorInt = toInt (elemAt vlist 1);
+      correctMinor = if isRC then minorInt - 1 else minorInt;
+    in "${elemAt vlist 0}.${toString correctMinor}";
+
+  directoryUrls = [
+    "mirror://kernel/linux/kernel/v4.x"
+    "mirror://kernel/linux/kernel/v4.x/testing"
+  ];
+
+  src = if source ? rev then
+    fetchFromGitHub {
+      inherit (source)
+        owner
+        repo
+        rev
+        sha256;
+    }
+  else
+    fetchurl {
+      urls =
+        let
+          version' = if source ? baseSha256 then unpatchedVersion else version;
+        in source.baseUrls or (source.urls or (map (n: "${n}/linux-${version'}.tar.xz") directoryUrls));
+      hashOutput = false;
+      sha256 = source.baseSha256 or source.sha256;
+    };
+
+  patch = if source ? patchSha256 then
+    fetchurl {
+      urls = source.patchUrls or (map (n: "${n}/patch-${version}.xz") directoryUrls);
+      hashOutput = false;
+      sha256 = source.patchSha256;
+    }
+  else
+    null;
+
+  srcsVerification = [
+    (fetchurl {
+      failEarly = true;
+      pgpDecompress = true;
+      pgpsigUrls = map (n: "${n}/linux-${if source ? baseSha256 then unpatchedVersion else version}.tar.sign") directoryUrls;
+      pgpKeyFingerprints = [
+        "647F 2865 4894 E3BD 4571  99BE 38DB BDC8 6092 693E"
+        "ABAF 11C6 5A29 70B1 30AB  E3C4 79BE 3E43 0041 1886"
+      ];
+      inherit (src) urls outputHash outputHashAlgo;
+    })
+  ] ++ optionals (patch != null) [
+    (fetchurl {
+      failEarly = true;
+      pgpDecompress = true;
+      pgpsigUrls = map (n: "${n}/patch-${version}.sign") directoryUrls;
+      pgpKeyFingerprints = [
+        "647F 2865 4894 E3BD 4571  99BE 38DB BDC8 6092 693E"
+        "ABAF 11C6 5A29 70B1 30AB  E3C4 79BE 3E43 0041 1886"
+      ];
+      inherit (patch) urls outputHash outputHashAlgo;
+    })
+  ];
+
+  lib = stdenv.lib;
+
+  modDirVersion = let
+    rcSplit = lib.splitString "-" version;
+    vSplit = lib.splitString "." (lib.head rcSplit);
+    vSplit' = if lib.length vSplit == 2 then vSplit ++ [ "0" ] else vSplit;
+    rcSplit' = [ (lib.concatStringsSep "." vSplit') ] ++ tail rcSplit;
+  in lib.concatStringsSep "-" rcSplit';
+
+  common = import ./common.nix { inherit stdenv; };
+
+  kernelConfigFun = baseConfig:
+    let
+      configFromPatches =
+        map ({extraConfig ? "", ...}: extraConfig) kernelPatches;
+    in lib.concatStringsSep "\n" ([baseConfig] ++ configFromPatches);
+
+  configfile = stdenv.mkDerivation {
+    inherit ignoreConfigErrors;
+    name = "linux-config-${version}";
+
+    generateConfig = ./generate-config.pl;
+
+    kernelConfig = kernelConfigFun config;
+
+    nativeBuildInputs = [ perl ]
+      ++ optionals needsGitPatch [ git ];
+
+    platformName = "pc";
+    kernelBaseConfig = "defconfig";
+    kernelTarget = "bzImage";
+    autoModules = true;
+    arch = common.kernelArch;
+
+    postPatch = kernel.postPatch + ''
+      # Patch kconfig to print "###" after every question so that
+      # generate-config.pl from the generic builder can answer them.
+      sed -e '/fflush(stdout);/i\printf("###");' -i scripts/kconfig/conf.c
+    '';
+
+    inherit (kernel) src patches preUnpack prePatch;
+
+    buildPhase = ''
+      cd $buildRoot
+
+      # Get a basic config file for later refinement with $generateConfig.
+      make -C ../$sourceRoot O=$PWD $kernelBaseConfig ARCH=$arch
+
+      # Create the config file.
+      echo "generating kernel configuration..."
+      echo "$kernelConfig" > kernel-config
+      DEBUG=1 ARCH=$arch KERNEL_CONFIG=kernel-config AUTO_MODULES=$autoModules \
+           SRC=../$sourceRoot perl -w $generateConfig
+    '';
+
+    installPhase = "mv .config $out";
+  };
+
+  kernel = buildLinux {
+    inherit version modDirVersion src needsGitPatch patch kernelPatches;
+
+    configfile = configfile.nativeDrv or configfile;
+
+    crossConfigfile = configfile.crossDrv or configfile;
+
+    config = { CONFIG_MODULES = "y"; CONFIG_FW_LOADER = "m"; };
+
+    crossConfig = { CONFIG_MODULES = "y"; CONFIG_FW_LOADER = "m"; };
+  };
+
+  passthru = {
+    meta = kernel.meta // extraMeta;
+
+    inherit srcsVerification;
+
+    features = source.features or { };
+
+    passthru = kernel.passthru // (removeAttrs passthru [ "passthru" "meta" ]);
+  };
+
+  config = import ./common-config.nix
+    { inherit stdenv version extraConfig; };
+
+  nativeDrv = lib.addPassthru kernel.nativeDrv passthru;
+
+  crossDrv = lib.addPassthru kernel.crossDrv passthru;
+in if kernel ? crossDrv then nativeDrv // { inherit nativeDrv crossDrv; } else lib.addPassthru kernel passthru
